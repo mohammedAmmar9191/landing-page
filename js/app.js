0b9c680e25b9124496409caa7cabf827de6a2cef
Mohammed Sayed <mohammed.ammar9191@gmail.com>
2023-02-04 18:19:29 +0300

    smooth scrolling

--- js/app.js
+++ js/app.js
@@ -44,6 +44,37 @@
 
 // build the nav
 
+const navMenu = document.querySelector("#navbar__list");
+const sections = document.querySelectorAll(".sec");
+const sectionOne = sections[0].getBoundingClientRect().top;
+const sectionTwo = sections[1].getBoundingClientRect().top;
+const sectionThree = sections[2].getBoundingClientRect().top;
+const sectionFour = sections[3].getBoundingClientRect().top;
+
+navMenu.addEventListener("click", function (e) {
+	if (e.target.nodeName === "A") {
+		e.preventDefault();
+		for (const section of sections) {
+			if (e.target.getAttribute("href").substring(1) === section.id) {
+				switch (section.id) {
+					case "sectionOne":
+						window.scrollTo({ top: sectionOne, behavior: "smooth" });
+						break;
+					case "sectionTwo":
+						window.scrollTo({ top: sectionTwo, behavior: "smooth" });
+						break;
+					case "sectionThree":
+						window.scrollTo({ top: sectionThree, behavior: "smooth" });
+                        break;
+                    case "sectionFour":
+                        window.scrollTo({ top: sectionFour, behavior: "smooth" });
+				}
+			}
+		}
+	}
+});
+
+
 
 // Add class 'active' to section when near top of viewport
 
